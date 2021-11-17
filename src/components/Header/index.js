8b8261480c6e636d280d9be88f3bcd70c37ccc95
Demetrius <wh46tesmoke@gmail.com>
2021-11-17 21:27:47 +0200

    add breadcrumb component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,11 +4,14 @@ import RMDBLogo from "../../images/react-movie-logo.svg";
 import TMBDLogo from "../../images/tmdb_logo.svg";
 
 import { Wrapper, Content, LogoImg, TMBDLogoImg } from "./Header.styles";
+import { Link } from "react-router-dom";
 
 const Header = () => (
   <Wrapper>
     <Content>
-      <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+      <Link to="./">
+        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+      </Link>
       <TMBDLogoImg src={TMBDLogo} alt="tmbdlogo" />
     </Content>
   </Wrapper>
